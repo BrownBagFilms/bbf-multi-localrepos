ca7ab37e8b53e6bd55b03620a588fd405b954595
Michor Lu <michor.lu@brownbagfilms.com>
2016-12-08 16:49:54 +0000

    Added support for bbf-conifg-override files.

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from sgtk.platform import Application
 
 class LocalRepos(Application):
     def init_app(self):
+
         app_module = self.import_module('bbf_multi_localrepos')
+        framework_sgutils = self.frameworks.get("tk-framework-shotgunutils")
+        settings_module = framework_sgutils.import_module("settings")
+        self.settings_manager = settings_module.UserSettings(self.engine)
 
         self.git = app_module.git
 
--- python/bbf_multi_localrepos/base.py
+++ python/bbf_multi_localrepos/base.py
@@ -14,7 +14,10 @@ import sys
 import threading
 from PySide import QtGui
 
+import bbf_sgtk_config.bbf_override_config as bbf_override_config
+
 import repoupdateui_ui
+
 
 class RepoUpdateUi(QtGui.QMainWindow):
     def __init__(self):
@@ -34,24 +37,46 @@ class RepoUpdateUi(QtGui.QMainWindow):
         self._ui.progressBar.setValue(percentage)
         QtGui.QApplication.processEvents()
 
-def update_local_repos(app):
-    def update(app, repo):
-        clone_success = app.execute_hook('hook_clone', msgBox=RepoUpdateUi, repo=repo)
-        if not clone_success:
-            app.log_info('Problem cloning {repo} repository'.format(repo=repo['name']))
 
-        update_success = app.execute_hook('hook_update', msgBox=RepoUpdateUi, repo=repo)
-        if not update_success:
-            app.log_info('Problem updating {repo} repository'.format(repo=repo['name']))
+def _update(app, repo):
+    project_id = app.context.project.get("id")
+    # Get stored override config file name
+    # config_file_name = app.settings_manager.retrieve("bbf_config_file_%s" % project_id, None,
+    #                                                  app.settings_manager.SCOPE_PROJECT)
+    config_file_name = bbf_override_config.get_config_file_name(project_id)
+
+    # Use override data if found
+    if config_file_name:
+        config_data = bbf_override_config.get_available_override_config(config_file_name)
+
+        if config_data:
+            repo_config_data = config_data.get("bbf_localrepos_config")
+
+            if repo_config_data:
+                repo["name"] = repo_config_data.get("name") or repo["name"]
+                repo["branch"] = repo_config_data.get("branch") or repo["branch"]
+                repo["local_url"] = repo_config_data.get("local_url") or repo["local_url"]
+                repo["remote_url"] = repo_config_data.get("remote_url") or repo["remote_url"]
+
+    clone_success = app.execute_hook('hook_clone', msgBox=RepoUpdateUi, repo=repo)
+    if not clone_success:
+        app.log_info('Problem cloning {repo} repository'.format(repo=repo['name']))
+
+    update_success = app.execute_hook('hook_update', msgBox=RepoUpdateUi, repo=repo)
+    if not update_success:
+        app.log_info('Problem updating {repo} repository'.format(repo=repo['name']))
+
+
+def update_local_repos(app):
 
     threads = []
     for repo in app.settings['local_repos']:
         if repo['as_background_process']:
-            t = threading.Thread(target=update, args=(app, repo))
+            t = threading.Thread(target=_update, args=(app, repo))
             threads.append(t)
             t.start()
         else:
-            update(app, repo)
+            _update(app, repo)
         
         if repo['add_to_pythonpath']:
             os.environ[repo['name']] = repo['local_url']
